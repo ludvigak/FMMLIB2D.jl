fbbe62f02817d2d2431c34467f1e17805ae2d851
Ludvig af Klinteberg <jafklint@sfu.ca>
2018-04-19 17:13:51 -0700

    Simplified code

--- src/FMMLIB2D.jl
+++ src/FMMLIB2D.jl
@@ -1,8 +1,3 @@
-# Interface to Fortran routines
-#
-# This package is probably leaking memory at the moment, since the Fortran routines
-# allocate memory themselves
-
 module FMMLIB2D
 
 export lfmm2dpartself, lfmm2dparttarg
@@ -13,7 +8,6 @@ if isfile(depsfile)
 else
     error("FMMLIB2D is not properly installed. Please run Pkg.build(\"FMMLIB2D\").")
 end
-
 
 function lfmm2dparttarg(iprec::Int64,
                         source::Array{Float64},
@@ -41,14 +35,14 @@ function lfmm2dparttarg(iprec::Int64,
     end
     ntarget = size(target, 2)
     @assert size(source, 1) == 2
-    # Prepare output structures
-    ier = zeros(Int64, 1)
-    pot = zeros(Complex128, nsource)
-    grad= zeros(Complex128, 2, nsource)
-    hess = zeros(Complex128, 3, nsource)
-    pottarg = Array{Complex128}(ntarget)
-    gradtarg = Array{Complex128}(2, ntarget)
-    hesstarg = Array{Complex128}(3, ntarget)
+    # Prepare output structures, only allocate those needed
+    ier = 0
+    pot = zeros(Complex128, nsource * (ifpot!=0 ? 1 : 0) )
+    grad = zeros(Complex128, 2, nsource * (ifgrad!=0 ? 1 : 0) )
+    hess = zeros(Complex128, 3, nsource * (ifhess!=0 ? 1 : 0) )
+    pottarg = Array{Complex128}(ntarget * (ifpottarg!=0 ? 1 : 0) )
+    gradtarg = Array{Complex128}(2, ntarget * (ifgradtarg!=0 ? 1 : 0) )
+    hesstarg = Array{Complex128}(3, ntarget * (ifhesstarg!=0 ? 1 : 0) )
     # Library call
     ccall( (:lfmm2dparttarg_, fmmlib2d), Void,
            (Ref{Int64}, # ier
@@ -81,12 +75,9 @@ function lfmm2dparttarg(iprec::Int64,
            ntarget,target,ifpottarg,pottarg,ifgradtarg,gradtarg,ifhesstarg,hesstarg
            )
     # Check and return
-    ier = ier[1]
     @assert ier == 0    
     return pot, grad, hess, pottarg, gradtarg, hesstarg
 end
-
-
 
 function lfmm2dpartself(iprec::Int64,
                         source::Array{Float64},
@@ -98,45 +89,13 @@ function lfmm2dpartself(iprec::Int64,
                         ifpot::Int64,
                         ifgrad::Int64,
                         ifhess::Int64)
-    nsource = size(source, 2)
-    @assert size(source, 1) == 2
-    if ifcharge != 0
-        @assert length(charge) == nsource
-    end
-    if ifdipole != 0
-        @assert length(dipstr) == nsource
-        @assert size(dipvec) == (2, nsource)
-    end    
-    ier = zeros(Int64, 1)
-    pot = zeros(Complex128, nsource)
-    grad= zeros(Complex128, 2, nsource)
-    hess = zeros(Complex128, 3, nsource)
-    pottarg = Array{Complex128}(1)
-    gradtarg = Array{Complex128}(2,1)
-    hesstarg = Array{Complex128}(3,1)    
-    ccall( (:lfmm2dpartself_, fmmlib2d), Void,
-           (Ref{Int64}, # ier
-            Ref{Int64}, # iprec
-            Ref{Int64}, # nsource
-            Ref{Float64}, # source
-            Ref{Int64}, # ifcharge
-            Ref{Complex128}, #charge
-            Ref{Int64}, # ifdipole
-            Ref{Complex128}, #dipstr
-            Ref{Float64}, #dipvec
-            Ref{Int64}, # ifpot
-            Ref{Complex128}, #pot
-            Ref{Int64}, # ifgrad
-            Ref{Complex128}, #grad
-            Ref{Int64}, # ifhess
-            Ref{Complex128} #hess
-            ),
-           ier,iprec,nsource,source,
-           ifcharge,charge,ifdipole,dipstr,dipvec,
-           ifpot,pot,ifgrad,grad,ifhess,hess           
-           )
-    ier = ier[1]
-    @assert ier == 0    
+    target = zeros(0,0)
+    ifpottarg = 0
+    ifgradtarg = 0
+    ifhesstarg = 0
+    pot, grad, hess, _, _, _ = 
+        lfmm2dparttarg(iprec, source, ifcharge, charge, ifdipole, dipstr, dipvec, ifpot, ifgrad, ifhess,
+                       target, ifpottarg, ifgradtarg, ifhesstarg)
     return pot, grad, hess
 end
 
