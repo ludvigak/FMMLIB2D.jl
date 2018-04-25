50871cf6a28451810c5c4a03c8c29ced5a47d21f
Ludvig af Klinteberg <jafklint@sfu.ca>
2018-04-25 13:40:37 -0700

    Extended tests

--- test/laplace.jl
+++ test/laplace.jl
@@ -6,29 +6,37 @@ srand(0)
 # TODO: Include dipoles
 # TODO: Compute gradient and hessian
 
-function direct_self(source, charge)
-    refpot = zeros(charge)
+function direct_self(source, charge, dipvec, dipstr)
+    pot = zeros(charge)
     N = size(source, 2)
     for i=1:N
         for j=1:N
             if i == j
                 continue
             end
-            refpot[i] += charge[j]*log(norm(source[:,i]-source[:,j]))
+            r1 = source[1,j]-source[1,i]
+            r2 = source[2,j]-source[2,i]
+            rsq = r1^2 + r2^2
+            rdotq = r1*dipvec[1,j] + r2*dipvec[2,j]            
+            pot[i] += charge[j]*log(rsq)/2 + dipstr[j]*rdotq/rsq
         end
     end
-    return refpot
+    return pot
 end
-function direct_targ(source, charge, target)
+function direct_targ(source, charge, dipvec, dipstr, target)
     M = size(target, 2)
     N = size(source, 2)
-    refpottarg = zeros(typeof(charge[1]), M)    
+    pot = zeros(typeof(charge[1]), M)    
     for i=1:M
         for j=1:N
-            refpottarg[i] += charge[j]*log(norm(target[:,i]-source[:,j]))
+            r1 = source[1,j]-target[1,i]
+            r2 = source[2,j]-target[2,i]
+            rsq = r1^2 + r2^2
+            rdotq = r1*dipvec[1,j] + r2*dipvec[2,j]            
+            pot[i] += charge[j]*log(rsq)/2 + dipstr[j]*rdotq/rsq
         end
     end
-    return refpottarg
+    return pot
 end    
 
 @testset "Laplace FMM 2D (complex)" begin
@@ -37,25 +45,26 @@ end
     M = 30
     source = rand(2, N)
     charge = rand(N) + 1im*rand(N)
-    dipstr = complex(rand(N))
+    dipstr = rand(N) + 1im*rand(N)
     dipvec = rand(2, N)
     target = rand(2, M)
     ifcharge = 1
-    ifdipole = 0
+    ifdipole = 1
     ifpot = 1
     ifgrad = 1
     ifhess = 1
     ifpottarg = 1
     ifgradtarg = 1
     ifhesstarg = 1
-    refpottarg = direct_targ(source, charge, target)
-    refpot = direct_self(source, charge)
+    refpottarg = direct_targ(source, charge, dipvec, dipstr, target)
+    refpot = direct_self(source, charge, dipvec, dipstr)
     @testset "Direct interface: lfmm2dpartself" begin
         pot, grad, hess = 
             lfmm2dpartself(iprec, source, ifcharge, charge, ifdipole, dipstr, dipvec, ifpot, ifgrad, ifhess)
         pot_relerr = norm(refpot-pot, Inf) / norm(refpot, Inf)
         @test pot_relerr < 1e-15
     end
+    
     @testset "Direct interface: lfmm2dparttarg" begin
         pot, grad, hess, pottarg, gradtarg, hesstarg = 
             lfmm2dparttarg(iprec, source, ifcharge, charge, ifdipole, dipstr, dipvec, ifpot, ifgrad, ifhess,
@@ -66,10 +75,25 @@ end
         @test pottarg_relerr < 1e-15
     end
     @testset "Keyword interface" begin
-        U = lfmm2d(source=source, charge=charge, target=target)
+        U = lfmm2d(source=source, charge=charge, dipstr=dipstr, dipvec=dipvec)
         @test norm(U.pot-refpot, Inf) / norm(refpot, Inf) < 1e-14
+        U = lfmm2d(source=source, charge=charge, dipstr=dipstr, dipvec=dipvec, target=target)        
         @test norm(U.pottarg-refpottarg, Inf) / norm(refpot, Inf) < 1e-14
-    end    
+    end
+    @testset "Tolerance test" begin
+        N = 2000
+        source = rand(2, N)
+        charge = rand(N) + 1im*rand(N)
+        dipstr = rand(N) + 1im*rand(N)
+        dipvec = rand(2, N)        
+        refpot = direct_self(source, charge, dipvec, dipstr)
+        for tolexp=-14:-1
+            TOL = 10.0^tolexp
+            U = lfmm2d(source=source, charge=charge, tol=TOL, dipstr=dipstr, dipvec=dipvec)
+            relerr = norm(U.pot-refpot) / norm(refpot)
+            @test relerr < TOL
+        end
+    end
 end
 
 @testset "Laplace FMM 2D (real)" begin
@@ -82,15 +106,15 @@ end
     dipvec = rand(2, N)
     target = rand(2, M)
     ifcharge = 1
-    ifdipole = 0
+    ifdipole = 1
     ifpot = 1
     ifgrad = 1
     ifhess = 1
     ifpottarg = 1
     ifgradtarg = 1
     ifhesstarg = 1
-    refpottarg = direct_targ(source, charge, target)
-    refpot = direct_self(source, charge)
+    refpottarg = direct_targ(source, charge, dipvec, dipstr, target)
+    refpot = direct_self(source, charge, dipvec, dipstr)
     @testset "Direct interface: rfmm2dpartself" begin
         pot, grad, hess = 
             rfmm2dpartself(iprec, source, ifcharge, charge, ifdipole, dipstr, dipvec, ifpot, ifgrad, ifhess)
@@ -107,8 +131,23 @@ end
         @test pottarg_relerr < 1e-15
     end
     @testset "Keyword interface" begin
-        U = rfmm2d(source=source, charge=charge, target=target)
+        U = rfmm2d(source=source, charge=charge, dipstr=dipstr, dipvec=dipvec)
         @test norm(U.pot-refpot, Inf) / norm(refpot, Inf) < 1e-14
+        U = rfmm2d(source=source, charge=charge, dipstr=dipstr, dipvec=dipvec, target=target)        
         @test norm(U.pottarg-refpottarg, Inf) / norm(refpot, Inf) < 1e-14
+    end
+    @testset "Tolerance test" begin
+        N = 2000
+        source = rand(2, N)
+        charge = rand(N)
+        dipstr = rand(N)
+        dipvec = rand(2, N)        
+        refpot = direct_self(source, charge, dipvec, dipstr)
+        for tolexp=-14:-1
+            TOL = 10.0^tolexp
+            U = rfmm2d(source=source, charge=charge, tol=TOL, dipstr=dipstr, dipvec=dipvec)
+            relerr = norm(U.pot-refpot) / norm(refpot)
+            @test relerr < TOL
+        end
     end    
 end
